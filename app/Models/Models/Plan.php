4553d16c3d629692eb5ee6c6754a8517892bd467
Obatista02 <guilherme.santos201412@gmail.com>
2022-03-18 19:56:20 -0300

    atualizando heroku

---
+++ app/Models/Models/Plan.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Plan extends Model
+{
+    protected $fillable = ['name', 'url', 'price', 'description'];
+
+    public function search($filter = null){
+        $results = $this
+                    ->where('name', 'LIKE', "%{$filter}%")
+                    ->orwhere('description', 'LIKE', "%{$filter}%")
+                    ->paginate();
+            return $results;
+    }
+
+}
